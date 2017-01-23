f9263b23f71ba7079baf0e072602155d92f79ed3
Brendan <mcb0193@comcast.net>
2017-01-23 11:55:45 -0600

    Add skeleton to caesar.py

---
+++ caesar.py
@@ -0,0 +1,14 @@
+import sys
+from helpers import rotate_char
+
+def encrypt(message, rot):
+	pass
+	
+def user_input_is_valid(cl_args):
+	pass
+	
+def main():
+	pass
+	
+if __name__ == "__main__":
+	main()
