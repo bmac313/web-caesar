c27f5395604e2663d271af07fac91514686d70b0
Brendan <mcb0193@comcast.net>
2017-01-25 21:20:24 -0600

    Implement alphabet_position function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,15 @@
+import string
+
 def alphabet_position(letter):
-	pass
+	""" 
+	Returns the index of the letter in the alphabet if it is in the alphabet.
+	Returns -1 if it is not.
+	"""
+	alphabet = string.ascii_uppercase
+	if letter.upper() in alphabet:
+		return alphabet.index(letter.upper())
+		
+	return -1
 	
 def rotate_char(char, rot):
 	pass
